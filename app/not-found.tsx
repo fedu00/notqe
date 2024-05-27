d5f5f00261a68b87596f81e12d639c729f41f329
Wojtek F <fedakwojciech@gmail.com>
2024-05-27 18:21:41 +0200

    create error 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import "./notFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="error-page">
+      <div className="error-container">
+        <p className="error-message">404</p>
+        <p className="error-text">Sorry, something went wrong!</p>
+      </div>
+    </div>
+  );
+}
