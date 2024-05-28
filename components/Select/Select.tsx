e436fb49af611ebf92b2ab39470b9e1fc2390685
Wojtek F <fedakwojciech@gmail.com>
2024-05-28 15:40:32 +0200

    add value to select component to prevent reset state bug

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -4,15 +4,15 @@ import "./Select.css";
 interface SelectType {
   data: string[];
   onChange(event: React.ChangeEvent<HTMLSelectElement>): void;
+  value: string;
 }
 
-export default function Select({ data, onChange }: SelectType) {
+export default function Select({ data, onChange, value }: SelectType) {
   return (
-    <select onChange={onChange}>
+    <select onChange={onChange} value={value}>
       {data.map((category: string) => (
         <option key={category}>{category}</option>
       ))}
-      <option></option>
     </select>
   );
 }
