5ede747d2acd0fad69ad17c0663981423af9d529
Wojtek F <fedakwojciech@gmail.com>
2024-12-03 16:49:09 +0100

    change url

--- apiClients/clientApi.ts
+++ apiClients/clientApi.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const clientApi = axios.create({
-  baseURL: "http://localhost:3000/api",
-  // just for build process, will be removed
-  // baseURL: `https://notqe.vercel.app/api/usersTasks?id=${id}`,
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_BASE_URL,
     "Access-Control-Allow-Credentials": "true",
   },
 });
